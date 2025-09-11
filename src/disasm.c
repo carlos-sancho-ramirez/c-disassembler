ede4b2e2f8a4e08dcbc6045b0f9a7285499034e3
Carlos Sancho Ramirez <carlos.sancho.ramirez@gmail.com>
2025-09-11 14:43:02 +0200

    Include dump of all opcodes from 0x00 to 0x5F

--- src/disasm.c
+++ src/disasm.c
@@ -38,30 +38,167 @@ static void print_literal_hex_word(void (*print)(const char *), int value) {
 	print(number);
 }
 
+static void print_differential_hex_byte(void (*print)(const char *), int value) {
+	char number[] = "+0x00";
+	if (value & 0x80) {
+		number[0] = '-';
+		value = (value ^ 0xFF) + 1;
+	}
+
+	number[4] = HEX_CHAR[value & 0x0F];
+	number[3] = HEX_CHAR[(value >> 4) & 0x0F];
+	print(number);
+}
+
+static void print_differential_hex_word(void (*print)(const char *), int value) {
+	char number[] = "+0x0000";
+	if (value & 0x8000) {
+		number[0] = '-';
+		value = (value ^ 0xFFFF) + 1;
+	}
+
+	number[6] = HEX_CHAR[value & 0x000F];
+	number[5] = HEX_CHAR[(value >> 4) & 0x000F];
+	number[4] = HEX_CHAR[(value >> 8) & 0x000F];
+	number[3] = HEX_CHAR[(value >> 12) & 0x000F];
+	print(number);
+}
+
+const char *BYTE_REGISTERS[] = {
+	"al", "cl", "dl", "bl", "ah", "ch", "dh", "bh"
+};
+
+const char *WORD_REGISTERS[] = {
+    "ax", "cx", "dx", "bx", "sp", "bp", "si", "di"
+};
+
+const char *INSTRUCTION[] = {
+	"add", "or", "adc", "sbb", "and", "sub", "xor", "cmp"
+};
+
+const char *ADDRESS_REGISTERS[] = {
+	"bx+si", "bx+di", "bp+si", "bp+di", "si", "di", "bp", "bx"
+};
+
+const char *SEGMENT_REGISTERS[] = {
+	"es", "cs", "ss", "ds"
+};
+
 static int dump_instruction(struct Reader *reader, void (*print)(const char *), void (*print_error)(const char *)) {
     const char *segment = NULL;
 	while (1) {
 		const int value0 = read_next_byte(reader);
-		if (value0 == 0x26) {
-			segment = "es:";
-		}
-		else if (value0 == 0x31) {
-			const int value1 = read_next_byte(reader);
-			if (value1 == 0xC0) {
-				print("xor ax,ax\n");
+		if (value0 >= 0 && value0 < 0x40 && (value0 & 0x06) != 0x06) {
+			print(INSTRUCTION[value0 >> 3]);
+			print(" ");
+			if ((value0 & 0x04) == 0x00) {
+				const char **registers;
+				if (value0 & 0x01) {
+					registers = WORD_REGISTERS;
+				}
+				else {
+					registers = BYTE_REGISTERS;
+				}
+
+				const int value1 = read_next_byte(reader);
+				if ((value0 & 0x06) == 0x00) {
+					if ((value1 & 0xC0) != 0xC0) {
+						print("[");
+						if (segment) {
+							print(segment);
+							print(":");
+						}
+
+						if ((value1 & 0xC7) == 0x06) {
+							print(read_next_word(reader));
+						}
+						else {
+							print(ADDRESS_REGISTERS[value1 & 0x07]);
+							if ((value1 & 0xC0) == 0x40) {
+								print_differential_hex_byte(reader, read_next_byte(reader));
+							}
+							else if ((value1 & 0xC0) == 0x40) {
+								print_differential_hex_word(reader, read_next_byte(reader));
+							}
+						}
+						print("],");
+					}
+					else {
+						print(registers[value1 & 0x07]);
+						print(",");
+					}
+					print(registers[(value1 >> 3) & 0x07]);
+				}
+				else if ((value0 & 0x06) == 0x02) {
+					print(registers[(value1 >> 3) & 0x07]);
+					print(",");
+
+					if ((value1 & 0xC0) != 0xC0) {
+						print("[");
+						if (segment) {
+							print(segment);
+							print(":");
+						}
+
+						if ((value1 & 0xC7) == 0x06) {
+							print(read_next_word(reader));
+						}
+						else {
+							print(ADDRESS_REGISTERS[value1 & 0x07]);
+							if ((value1 & 0xC0) == 0x40) {
+								print_differential_hex_byte(print, read_next_byte(reader));
+							}
+							else if ((value1 & 0xC0) == 0x40) {
+								print_differential_hex_word(print, read_next_byte(reader));
+							}
+						}
+						print("]");
+					}
+					else {
+						print(registers[value1 & 0x07]);
+					}
+				}
+				print("\n");
 				return 0;
 			}
-			else {
-				print_error("Unknown opcode ");
-				print_literal_hex_byte(print_error, value0);
-				print_error(" ");
-				print_literal_hex_byte(print_error, value1);
-				print_error("\n");
-				return 1;
+			else if ((value0 & 0x07) == 0x04) {
+				print(BYTE_REGISTERS[0]);
+				print(",");
+				print_literal_hex_byte(print, read_next_byte(reader));
+				print("\n");
+				return 0;
+			}
+			else if ((value0 & 0x07) == 0x05) {
+				print(WORD_REGISTERS[0]);
+				print(",");
+				print_literal_hex_word(print, read_next_word(reader));
+				print("\n");
+				return 0;
 			}
 		}
-		else if (value0 == 0x48) {
-			print("dec ax\n");
+		else if ((value0 & 0xE6) == 0x06 && value0 != 0x0F) {
+			if (value0 & 0x01) {
+				print("pop ");
+			}
+			else {
+				print("push ");
+			}
+			print(SEGMENT_REGISTERS[(value0 >> 3) & 0x03]);
+			print("\n");
+			return 0;
+		}
+		else if ((value0 & 0xE7) == 0x26) {
+			segment = SEGMENT_REGISTERS[(value0 >> 3) & 0x03];
+		}
+		else if ((value0 & 0xF0) == 0x40) {
+			if (value0 & 0x08) {
+				print("dec ");
+			}
+			else {
+				print("inc ");
+			}
+			print(WORD_REGISTERS[value0 & 0x07]);
+			print("\n");
 			return 0;
 		}
 		else if (value0 == 0x8E) {
@@ -83,6 +220,7 @@ static int dump_instruction(struct Reader *reader, void (*print)(const char *), 
 			print("mov ax,[");
 			if (segment) {
 				print(segment);
+				print(":");
 			}
 			print_literal_hex_word(print, read_next_word(reader));
 			print("]\n");
